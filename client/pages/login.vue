524d364a0a84aad7bc4018744218074b497edd09
Emon Khan <bdemon00@gmail.com>
2021-04-11 11:22:49 +0600

    first commit

---
+++ client/pages/login.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <div class="col-md-4">
+      <button v-if="!$auth.loggedIn" @click.prevent="login" class="btn btn-primary mt-2">Login</button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+
+  methods: {
+    login() {
+      this.$auth.loginWith('laravelSanctum', {
+        data: {
+          email :'user@mail.com',
+          password:'user@mail.com'
+        }
+      })
+    },
+  },
+};
+</script>
+
+<style>
+</style>
